8f5e6d9a27c275b7f27ef96480c63c37051ec4f8
Jude Agboola <marvinjudehk@gmail.com>
2020-06-10 21:25:47 +0100

    fix issue with private key formatting

--- utils/populatedb.js
+++ utils/populatedb.js
@@ -21,7 +21,7 @@ async function populatedb() {
   const [connectError] = await to(
     doc.useServiceAccountAuth({
       client_email: process.env.CLIENT_EMAIL,
-      private_key: process.env.PRIVATE_KEY,
+      private_key: process.env.PRIVATE_KEY.replace(/\\n/gm, "\n"),
     })
   );
 
